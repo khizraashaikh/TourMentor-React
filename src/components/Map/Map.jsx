4284c7a9dc3ae7a9874b4bac21f74af5aff6731c
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-05-06 16:42:33 +0530

    Map Component Done.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import { LocationOnOutlined } from '@material-ui/icons';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
 
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+    const coordiantes = { lat: 0, lng: 0 };
+    
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'Your-api-key-here-console.cloud.google.com' }}
+                defaultCenter={coordiantes}
+                center={coordiantes}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
     );
 };
 
