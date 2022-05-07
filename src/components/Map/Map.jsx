7ff7a21ad92350ae1c9a7a4f0d0cb42f84cce668
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-05-07 19:06:33 +0530

    Scroll Places on Place Click & Filters Done.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import { LocationOnOutlined } from '@material-ui/icons';
@@ -6,7 +6,7 @@ import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles';
 
-const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
+const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked }) => {
     const classes = useStyles();
     const isDesktop = useMediaQuery('(min-width:600px)');
     
@@ -24,7 +24,7 @@ const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
                     setCoordinates({lat: e.center.lat, lng: e.center.lng });
                     setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
                 }}
-                onChildClick={''}
+                onChildClick={(child) => setChildClicked(child)}
             >
                 {places?.map((place, i) => (
                     <div
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -6,9 +6,11 @@ import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles';
 
-const PlaceDetails = ({ place }) => {
+const PlaceDetails = ({ place, selected, refProp }) => {
     const classes = useStyles();
-    
+
+    if (selected) refProp?.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+
     return (
         <Card elevation={6}>
             <CardMedia 
@@ -31,7 +33,7 @@ const PlaceDetails = ({ place }) => {
                     <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
                 </Box>
                 {place?.awards?.map((award) => (
-                    <Box my={1} display="flex" justifyContent="space-between" alignItems="center">
+                    <Box key={award.display_name} my={1} display="flex" justifyContent="space-between" alignItems="center">
                         <img src={award.images.small} alt={award.display_name} />
                         <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
                     </Box>
