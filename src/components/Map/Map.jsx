634780f8145d83135c6988b7fabeeada125e3495
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-05-08 16:49:43 +0530

    Map Styles Changed.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,6 +6,8 @@ import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles';
 
+import mapStyles from './mapStyles';
+
 const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked, weatherData }) => {
     const classes = useStyles();
     const isDesktop = useMediaQuery('(min-width:600px)');
@@ -18,7 +20,7 @@ const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked, 
                 center={coordinates}
                 defaultZoom={14}
                 margin={[50, 50, 50, 50]}
-                options={''}
+                options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
                 onChange={(e) => {
                     setCoordinates({lat: e.center.lat, lng: e.center.lng });
                     setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
