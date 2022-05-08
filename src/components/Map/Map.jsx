297c57a53cf56fd83c701f9a9b90c85d78cde1d4
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-05-08 10:15:42 +0530

    Search Feature Implemented.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -20,7 +20,6 @@ const Map = ({ setCoordinates, setBounds, coordinates, places, setChildClicked }
                 margin={[50, 50, 50, 50]}
                 options={''}
                 onChange={(e) => {
-                    console.log(e);
                     setCoordinates({lat: e.center.lat, lng: e.center.lng });
                     setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
                 }}
